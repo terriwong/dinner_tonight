2759f463d25156c2570031f0077e80996f14d2c2
Terri Wong <terriwmj@gmail.com>
2016-01-24 23:50:54 -0800

    added more items to dict, added address in jinja, updated on result page.

--- restaurants.py
+++ restaurants.py
@@ -2,6 +2,7 @@ berkeley_restaurant_week = {
     "1": {
         "name": "44 Restaurant, Bar & Lounge",
         "neighborhood": "Ashby Arts District",
+        "address": "3290 Alcatraz Ave, Berkeley, CA 94703",
         "type": "American",
         "price": 35,
         "reservable": False
@@ -9,6 +10,7 @@ berkeley_restaurant_week = {
     "2": {
         "name": "AKEMI Japanese Cuisine",
         "neighborhood": "Solano Ave",
+        "address": "1695 Solano Ave, Berkeley, CA 94707",
         "type": "Japanese",
         "price": 25,
         "reservable": True
@@ -16,6 +18,7 @@ berkeley_restaurant_week = {
     "3": {
         "name": "Bay Grille",
         "neighborhood": "West Berkeley",
+        "address": "200 Marina Blvd, Berkeley, CA 94710",
         "type": "American",
         "price": 35,
         "reservable": True
@@ -23,6 +26,7 @@ berkeley_restaurant_week = {
     "4": {
         "name": "Bistro Liaison",
         "neighborhood": "Downtown Berkeley",
+        "address": "1849 Shattuck Ave, Berkeley, CA 94709",
         "type": "French",
         "price": 35,
         "reservable": False
@@ -30,6 +34,7 @@ berkeley_restaurant_week = {
     "5": {
         "name": "Comal",
         "neighborhood": "Downtown Berkeley",
+        "address": "2020 Shattuck Ave, Berkeley, CA 94704",
         "type": "Mexican",
         "price": 35,
         "reservable": False
@@ -37,6 +42,7 @@ berkeley_restaurant_week = {
     "6": {
         "name": "Corso",
         "neighborhood": "Gourmet Ghetto",
+        "address": "1788 Shattuck Ave, Berkeley, CA 94709",
         "type": "Italian",
         "price": 25,
         "reservable": True
@@ -44,6 +50,7 @@ berkeley_restaurant_week = {
     "7": {
         "name": "Easy Creole",
         "neighborhood": "Ashby Arts District",
+        "address": "1761 Alcatraz Avenue, Berkeley, CA 94703",
         "type": "Cajun",
         "price": 12.5,
         "reservable": False
@@ -51,6 +58,7 @@ berkeley_restaurant_week = {
     "8": {
         "name": "FIVE",
         "neighborhood": "Downtown Berkeley",
+        "address": "2086 Allston Way, Berkeley, CA 94704",
         "type": "American",
         "price": 35,
         "reservable": True
@@ -58,6 +66,7 @@ berkeley_restaurant_week = {
     "9": {
         "name": " Gaumenkitzel Restaurant, Beer & Wine Bar",
         "neighborhood": "West Berkeley",
+        "address": "2121 San Pablo Ave, Berkeley, CA 94702",
         "type": "German",
         "price": 25,
         "reservable": False
@@ -65,8 +74,89 @@ berkeley_restaurant_week = {
     "10": {
         "name": "Hs Lordship's Restaurant",
         "neighborhood": "West Berkeley",
+        "address": "199 Seawall Dr, Berkeley, CA 94710",
         "type": "American",
         "price": 35,
         "reservable": True
-        }
+        },
+    "11": {
+        "name": "Julia's at Berkeley City Club",
+        "neighborhood": "Telegraph Ave",
+        "address": "2315 Durant Ave. Berkeley, CA 94704",
+        "type": "American",
+        "price": 35,
+        "reservable": True
+        },
+    "12": {
+        "name": "Longbranch Saloon",
+        "neighborhood": "West Berkeley",
+        "address": "2515 San Pablo Avenue, Berkeley, CA 94702",
+        "type": "American",
+        "price": 35,
+        "reservable": True
+        },
+    "13": {
+        "name": "Mission Heirloom",
+        "neighborhood": "Gourmet Ghetto",
+        "address": "2085 Vine Street, Berkeley, CA 94709",
+        "type": "Organic",
+        "price": 20,
+        "reservable": False
+        },
+    "14": {
+        "name": "Nico's Hideaway",
+        "neighborhood": "Gourmet Ghetto",
+        "address": "1508B Walnut St. Berkeley, CA 94709",
+        "type": "Bar",
+        "price": 35,
+        "reservable": False
+        },
+    "15": {
+        "name": "Pathos Organic Greek Kitchen",
+        "neighborhood": "Downtown Berkeley",
+        "address": "2430 Shattuck Ave, Berkeley, CA 94704",
+        "type": "Greek",
+        "price": 35,
+        "reservable": True
+        },
+    "16": {
+        "name": "Revival Bar + Kitchen",
+        "neighborhood": "Downtown Berkeley",
+        "address": "2102 Shattuck Ave, Berkeley, CA 94704",
+        "type": "Northern California",
+        "price": 35,
+        "reservable": True
+        },
+    "17": {
+        "name": "Riva Cucina",
+        "neighborhood": "West Berkeley",
+        "address": "800 Heinz Ave, Berkeley, CA 94710",
+        "type": "Northern Italian",
+        "price": 35,
+        "reservable": True
+        },
+    "18": {
+        "name": "Skates On The Bay",
+        "neighborhood": "West Berkeley",
+        "address": "100 Seawall Drive, Berkeley, CA 94710",
+        "type": "Seafood",
+        "price": 35,
+        "reservable": True
+        },
+    "19": {
+        "name": "Rivoli Restaurant",
+        "neighborhood": "Solano Ave",
+        "address": "1539 Solano Ave, Berkeley, CA 94709",
+        "type": "Organic",
+        "price": 35,
+        "reservable": True
+        },
+    "20": {
+        "name": "Spenger's Fresh Fish Grotto",
+        "neighborhood": "Fourth Street",
+        "address": "1919 Fourth St, Berkeley, CA 94710",
+        "type": "Seafood",
+        "price": 35,
+        "reservable": True
+        },
 }
